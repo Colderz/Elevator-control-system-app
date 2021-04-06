569d8018850cef6a233ae0126472bb7b8571aca6
Colderz <arekzim98@gmail.com>
2021-04-06 20:45:48 +0200

    Initialize repository

---
+++ app/src/main/java/com/arkadiuszzimny/elevatorcontrolsystemapp/ElevatorActivity.java
@@ -0,0 +1,14 @@
+package com.arkadiuszzimny.elevatorcontrolsystemapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElevatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
