a373785acec2c7b2a9460530f10c3bfc4267398f
Colderz <arekzim98@gmail.com>
2021-04-10 13:12:18 +0200

    Support for simulation for all elevators, blockade after reaching the last floor of the queue.

--- app/src/main/java/com/arkadiuszzimny/elevatorcontrolsystemapp/ui/PanelFragmentViewModel.java
+++ app/src/main/java/com/arkadiuszzimny/elevatorcontrolsystemapp/ui/PanelFragmentViewModel.java
@@ -31,23 +31,29 @@ public class PanelFragmentViewModel extends AndroidViewModel {
     }
 
     public void stepSimulation(List<ElevatorItem> elevatorItems) {
-            upsert(new ElevatorItem(1, Integer.parseInt(String.valueOf(elevatorItems.get(0).getTargetFloors().get(0))), step(elevatorItems.get(0).getCurrentFloor(), elevatorItems.get(0).getTargetFloors()), elevatorItems.get(0).getMaxFloor(), setState(elevatorItems)));
-            //step(elevatorItems.get(0).getCurrentFloor(), elevatorItems.get(0).getTargetFloors());
+        //upsert(new ElevatorItem(1, Integer.parseInt(String.valueOf(elevatorItems.get(0).getTargetFloors().get(0))), step(elevatorItems.get(0).getCurrentFloor(), elevatorItems.get(0).getTargetFloors()), elevatorItems.get(0).getMaxFloor(), setState(elevatorItems)));
+        for(ElevatorItem item : elevatorItems) {
+            if(item.getState() == 0 && String.valueOf(item.getTargetFloors().get(0)).equals("-1")) {
+                upsert(new ElevatorItem(item.getId(), item.getCurrentFloor(), item.getTargetFloors(), item.getMaxFloor(), 0));
+            } else {
+                upsert(new ElevatorItem(item.getId(), Integer.parseInt(String.valueOf(item.getTargetFloors().get(0))), step(item.getCurrentFloor(), item.getTargetFloors()), item.getMaxFloor(), setState(item)));
+            }
+        }
     }
 
     private ArrayList<String> step(int currLevel, ArrayList<String> queue) {
-        if(queue.size()>=1) {
+        if (queue.size() >= 1) {
             queue.remove(0);
         }
-        if(queue.size()==0) {
+        if (queue.size() == 0) {
             queue.add("-1");
         }
         return queue;
     }
 
-    private int setState(List<ElevatorItem> elevatorItems) {
-        if(String.valueOf(elevatorItems.get(0).getTargetFloors().get(0)).equals("-1")) return 0;
-        else return elevatorItems.get(0).getState();
+    private int setState(ElevatorItem item) {
+        if (String.valueOf(item.getTargetFloors().get(0)).equals("-1")) return 0;
+        else return item.getState();
     }
 
 
